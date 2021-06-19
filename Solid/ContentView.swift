549b18a40f65170e2a7e652715c603d7dee2f49a
Andrew Sawyer <ajsawyer3@gmail.com>
2021-06-18 23:13:38 -0400

    Initial Commit

---
+++ Solid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Solid
+//
+//  Created by Andrew Sawyer on 6/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Solid/SolidApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolidApp.swift
+//  Solid
+//
+//  Created by Andrew Sawyer on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct SolidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
